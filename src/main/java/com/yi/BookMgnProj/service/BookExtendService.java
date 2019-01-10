908ee6f5c56162e09623d255d7f42b57e85b9385
krilln <krilln@naver.com>
2019-01-10 18:06:33 +0900

    hgs

--- src/main/java/com/yi/BookMgnProj/service/BookExtendService.java
+++ src/main/java/com/yi/BookMgnProj/service/BookExtendService.java
@@ -1,5 +1,25 @@
 package com.yi.BookMgnProj.service;
 
-public class BookExtendService {
+import com.yi.BookMgnProj.dao.BookMapper;
+import com.yi.BookMgnProj.dao.BookMapperImpl;
+import com.yi.BookMgnProj.dao.BookRentalInfoMapper;
+import com.yi.BookMgnProj.dao.BookRentalInfoMapperImpl;
+import com.yi.BookMgnProj.model.Book;
+import com.yi.BookMgnProj.model.BookRentalInfo;
 
+public class BookExtendService {
+	private BookRentalInfoMapper bookRentalInfoMapper = BookRentalInfoMapperImpl.getInstance();
+	private BookMapper bookMapper = BookMapperImpl.getInstance();
+	
+	public BookRentalInfo selectRentalNoByBookCode_returnDateNull(BookRentalInfo bookRentalInfo){
+		return bookRentalInfoMapper.selectRentalNoByBookCode_returnDateNull(bookRentalInfo);
+	}
+	
+	public int updateReturnDate(BookRentalInfo bookRentalInfo){
+		return bookRentalInfoMapper.updateReturnDate(bookRentalInfo);
+	}
+	
+	public Book selectBookBybookCodeOne(Book book){
+		return bookMapper.selectBookBybookCodeOne(book);
+	}
 }
