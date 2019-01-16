7b929ccc37ed6805c0d80a2e675c7d8f3b6be70c
krilln <krilln@naver.com>
2019-01-16 10:13:17 +0900

    수정

--- src/main/java/com/yi/BookMgnProj/handler/HomeHandler.java
+++ src/main/java/com/yi/BookMgnProj/handler/HomeHandler.java
@@ -18,8 +18,7 @@ public class HomeHandler implements CommandHandler {
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		//error 수정필요
-		/*Map<String, Object> map = new HashMap<>();
+		Map<String, Object> map = new HashMap<>();
 		if(req.getMethod().equalsIgnoreCase("get")){
 			Best3Service service = new Best3Service();
 			Date fristrentalDate = new Date();
@@ -29,34 +28,16 @@ public class HomeHandler implements CommandHandler {
 			map.put("fristrentalDate", fristrentalDate);
 			map.put("lastrentalDate", lastrentalDate);
 			SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
-			
-			String dates = "['제목','"+ date.format(fristrentalDate) +"~"+date.format(lastrentalDate)+"','";
-			
+		
 			List<BookBest10> list = service.selectBookBest10ByMap(map);
 			req.setAttribute("list", list);
 			
 			req.setAttribute("fristrentalDate", date.format(fristrentalDate));
 			req.setAttribute("lastrentalDate", date.format(lastrentalDate));
 
-			
-			
-			fristrentalDate.setDate(fristrentalDate.getDate()-7);
-			lastrentalDate.setDate(lastrentalDate.getDate()-7);
-			
-			dates = dates + date.format(fristrentalDate) +"~"+date.format(lastrentalDate)+"','"+"'평균']";
-			
-			map.put("fristrentalDate", fristrentalDate);
-			map.put("lastrentalDate", lastrentalDate);
-			List<String> bookCodes = new ArrayList<>();
-			for(BookBest10 best10 : list){
-				bookCodes.add(best10.getBookCode().getBookCode());
-			}
-			map.put("bookCodes", bookCodes);
-			List<BookBest10> list2 = service.selectBookBest10ByMap(map);
-			req.setAttribute("list2", list2);
-			
+		
 			return "/WEB-INF/view/home.jsp";
-		}*/
+		}
 		
 		return "/WEB-INF/view/home.jsp";
 	}
